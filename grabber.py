bf555c24ce5cb069ca0fdc7a9f682345ab3147b8
Saulo Aflitos <sauloal@gmail.com>
2013-08-09 12:26:46 +0200

    command to play youtube stream

--- grabber.py
+++ grabber.py
@@ -253,3 +253,7 @@ def main():
 
 
 if __name__ == "__main__": main()
+
+#url='http://r18---sn-5hn7sner.c.youtube.com/videoplayback?ratebypass=yes&expire=1376066979&mt=1376043762&ipbits=8&fexp=931318%2C916903%2C920508%2C904448%2C926103%2C916625%2C919515%2C909546%2C906397%2C916914%2C929117%2C929121%2C929906%2C929907%2C929127%2C929129%2C929131%2C929930%2C925720%2C925722%2C925718%2C925714%2C929917%2C929919%2C912521%2C925302%2C932306%2C920605%2C904830%2C919373%2C904122%2C929609%2C911423%2C909549%2C900816%2C912711%2C935802%2C904494%2C906001&id=ffeea127cb25b5d2&key=yt1&source=youtube&mv=m&ms=au&itag=22&upn=tCcuU6PLA_w&ip=95.96.159.83&sparams=cp%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&cp=U0hWS1BRVF9MU0NONl9IS1hKOm1feE43cEhDeFZs&sver=3&signature=36C9035D814C99EC06B0791DD89C7A9FE005DB70.7497782C3084DF60B76C5E1292D468B3D714DFBC'
+# payload='{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "'${url}'" }}, "id": 1 }'
+#curl -v -d "$payload" -H "Content-type:application/json" -X POST "127.0.0.1:8080/jsonrpc"
